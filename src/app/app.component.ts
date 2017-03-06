3edb0a7ff5a4b7ad8d99a0262d323bc903da303f
abbes_yassine <abbesyassine4@gmail.com>
2017-03-06 14:16:27 +0100

    Integrating angular2 Template Admin

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
-import {Component} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
-@Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+@Component( {
+    selector: 'app-root',
+    templateUrl: './app.component.html'
 })
-export class AppComponent {
-  title = 'Hello Oussema!';
+export class AppComponent implements OnInit {
+    constructor( ) {
+      // TODO
+    }
+
+    public ngOnInit() {
+      // TODO
+    }
+
 }
