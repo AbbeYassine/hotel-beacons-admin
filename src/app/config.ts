75f23062f61b06e443f37d131ad1b6d7ed1216ae
abbes_yassine <abbesyassine4@gmail.com>
2017-03-09 13:53:12 +0100

    change baseUrl

--- src/app/config.ts
+++ src/app/config.ts
@@ -2,5 +2,5 @@
  * Created by Vyndee on 08/03/2017.
  */
 export class Config {
-  public static apiUrl = "http://localhost:3000/api";
+  public static apiUrl = "http://ec2-34-251-71-9.eu-west-1.compute.amazonaws.com:3000/api";
 }
