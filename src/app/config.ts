e2438517caa91d40390846e8f995e9b03629f201
abbes_yassine <abbesyassine4@gmail.com>
2017-04-11 11:09:44 +0100

    fixing url

--- src/app/config.ts
+++ src/app/config.ts
@@ -2,5 +2,5 @@
  * Created by Vyndee on 08/03/2017.
  */
 export class Config {
-    public static apiUrl = "http://ec2-34-253-143-92.eu-west-1.compute.amazonaws.com:8080/api";
+    public static apiUrl = "http://ec2-52-50-48-84.eu-west-1.compute.amazonaws.com:8080/api";
 }
